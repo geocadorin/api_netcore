c10e9fa9801bcdbb82956687c386b315e597250a
geocadorin <geeo.cadorin@gmail.com>
2024-03-31 09:11:34 -0300

    add git ignore

--- src/Api.CrossCutting/obj/Debug/netcoreapp3.1/CrossCutting.AssemblyInfo.cs
+++ src/Api.CrossCutting/obj/Debug/netcoreapp3.1/CrossCutting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrossCutting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8c84bcbc946595e4c7c3e38e35bbaaf48eb897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d035d66a39c098a5054af9e93fc6313e6aa8dd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrossCutting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrossCutting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Api.Service/obj/Debug/netcoreapp3.1/Service.AssemblyInfo.cs
+++ src/Api.Service/obj/Debug/netcoreapp3.1/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8c84bcbc946595e4c7c3e38e35bbaaf48eb897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d035d66a39c098a5054af9e93fc6313e6aa8dd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
