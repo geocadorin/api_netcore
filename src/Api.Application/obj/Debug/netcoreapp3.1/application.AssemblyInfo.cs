ddb4d9409cd2850ebe0b90dc00489871e7a6296f
geocadorin <geeo.cadorin@gmail.com>
2024-03-31 10:28:11 -0300

    Created UsersController

--- src/Api.Application/obj/Debug/netcoreapp3.1/application.AssemblyInfo.cs
+++ src/Api.Application/obj/Debug/netcoreapp3.1/application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebbc56d9df09bd6a198af27fbea3e7f3794865f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07fe5c42e6f0fe49ef250b13261427d5d738dcf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
