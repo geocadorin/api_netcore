ebbc56d9df09bd6a198af27fbea3e7f3794865f5
geocadorin <geeo.cadorin@gmail.com>
2024-03-31 09:47:45 -0300

    IUserService implementation

--- src/Api.Service/obj/Debug/netcoreapp3.1/Service.AssemblyInfo.cs
+++ src/Api.Service/obj/Debug/netcoreapp3.1/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+812bc82830ee91b03f7d6f265df21cae23056597")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033c905fe9fe4c01d24640c8953f918055dd26d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
