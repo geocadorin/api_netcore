07fe5c42e6f0fe49ef250b13261427d5d738dcf2
geocadorin <geeo.cadorin@gmail.com>
2024-03-31 09:53:23 -0300

    added Api.Application references to Api.CrossCutting, Api.Domain and Api.Services

--- src/Api.Data/obj/Debug/netcoreapp3.1/Data.AssemblyInfo.cs
+++ src/Api.Data/obj/Debug/netcoreapp3.1/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+812bc82830ee91b03f7d6f265df21cae23056597")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebbc56d9df09bd6a198af27fbea3e7f3794865f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
