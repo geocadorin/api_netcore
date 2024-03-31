812bc82830ee91b03f7d6f265df21cae23056597
geocadorin <geeo.cadorin@gmail.com>
2024-03-31 09:27:45 -0300

    Created user service interface

--- src/Api.Domain/obj/Debug/netcoreapp3.1/Domain.AssemblyInfo.cs
+++ src/Api.Domain/obj/Debug/netcoreapp3.1/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d035d66a39c098a5054af9e93fc6313e6aa8dd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c10e9fa9801bcdbb82956687c386b315e597250a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
